8d2be29162eda40d1ae53c655d77407c5ec658c1
Aviral09 <aviralgoel2001@gmail.com>
2021-10-07 04:45:36 +0530

    Lists video 18

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,39 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: QuoteList(),
   ));
 }
 
-class Home extends StatefulWidget {
-    const Home({Key? key}) : super(key: key);
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
 
-    @override
-    _HomeState createState() => _HomeState();
-  }
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
 
-class _HomeState extends State<Home>{
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(quote: "Hello man",author :"Aviral"),
+    Quote(quote: "How are you man?",author : "Aviral"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        backgroundColor: Colors.grey[850],
-        title: const Text(
-          "Ninja ID Card",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
+        title: Text('Quotes'),
         centerTitle: true,
-        elevation: 0,
+        backgroundColor: Colors.lightBlueAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            ninjaLevel+=1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[850],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 30),
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/space.jpg'),
-                radius: 30,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Divider(height: 60,
-            color: Colors.grey[850],
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              )
-            ),
-            SizedBox(height: 10),
-            Text(
-                'Aviral',
-                style: TextStyle(
-                    color: Colors.amber[200],
-                    letterSpacing: 2.0,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                )
-            ),
-            SizedBox(height: 30),
-            Text(
-                'CURRENT NINJA LEVEL',
-                style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0
-                )
-            ),
-            SizedBox(height: 10),
-            Text(
-                '$ninjaLevel',
-                style: TextStyle(
-                  color: Colors.amber[200],
-                  letterSpacing: 2.0,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                )
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: const [
-                Icon(Icons.mail,
-                color: Colors.grey),
-                SizedBox(width: 10),
-                Text('aviralgoel2001@gmail.com',
-                style: TextStyle(
-                  color: Colors.grey
-                )
-                )
-              ],
-            )
-          ],
+          children: quotes.map((quote) => Text('${quote.quote} - ${quote.author}')).toList(),
         ),
       )
     );
---
+++ lib/quote.dart
@@ -0,0 +1,10 @@
+class Quote{
+  String quote="";
+  String author="";
+
+  // Quote(String quote, String author){
+  //   this.quote = quote;
+  //   this.author = author;
+  // }
+  Quote({required this.quote, required this.author});
+}
